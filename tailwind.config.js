85ee5c955aa09c20cf5b66c2d423eb94c2921e63
Bernhard Waitz <bernhard.waitz@oegbverlag.at>
2024-06-14 16:17:45 +0200

    WEB-1183: clean up and proxima nova

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js,scss}'],
   theme: {
+    screens: {
+      tablet: '767px',
+      desktop: '992px',
+      site: '1024px',
+    },
+    fontFamily: {
+      body: ['Arial', 'sans-serif'],
+      headline: ['Proxima Nova', 'Arial', 'sans-serif'],
+      menu: ['Proxima Nova', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
